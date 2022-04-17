e8099fff4a0bdd69c1a2363d2a30c9ac7eaff7f3
nakai540216 <nakai540216@gmail.com>
2022-04-17 16:57:54 +0900

    add new file

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def new
+    @post=Post.new(content:params[:content])
+  end
+  
+  def create
+    # フォームから送信されたデータを受け取り、保存する処理を追加してください
+    @post=Post.new(content:params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+end
